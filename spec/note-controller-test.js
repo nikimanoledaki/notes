6046ffb2e326500e8fd155f937a948ae6a61cd18
niki-01 <niki.manoledaki@gmail.com>
2020-02-05 15:41:38 +0000

    Tests that controller creates HTML string

--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -7,5 +7,42 @@
     console.log("Test passes")
   }
 
-})()
+})();
 
+(function TestControllerAddsNote() {
+  var controller = new Controller()
+  controller.addNote("Hi")
+
+  if (controller.list.array[0] !== "Hi") {
+    throw new Error("Assertion is not truthy")
+  } else {
+    console.log("Test passes")
+  }
+})();
+
+(function TestControllerInputsArray() {
+  var controller = new Controller()
+  controller.addNote("Hi")
+  controller.inputArray()
+
+  if (controller.view.array.length !== 1) {
+    throw new Error("Assertion is not truthy")
+  } else if (controller.view.array[0] !== "Hi") {
+    throw new Error("Assertion is not truthy")
+  } 
+  else {
+    console.log("Test passes")
+  }
+})();
+
+(function TestControllerCreatesString() {
+  var controller = new Controller()
+  controller.addNote("Hi")
+  controller.inputArray()
+
+  if (controller.createString() !== "<ul><li><div>Hi</div></li></ul>") {
+    throw new Error("Assertion is not truthy")
+  } else {
+    console.log("Test passes")
+  }
+})();
